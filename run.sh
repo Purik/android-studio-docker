3629ce58fea1d8e761c21729ba9a0f2dc8d66f8b
Павел <minikspb@gmail.com>
2017-11-02 18:01:01 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -it --net=host --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" purik/android-studio:latest /opt/android-studio/bin/studio.sh
