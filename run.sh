439eb636f39f95d097acbe2e80d5e79cab59a041
Nicholas Albion <nalbion@yahoo.com>
2019-10-10 14:36:44 +1100

    current version is AndroidStudio3.4

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ adb kill-server
 docker run -it --net=host --env="DISPLAY" \
   --privileged -v /dev/bus/usb:/dev/bus/usb \
   -v /tmp/.X11-unix:/tmp/.X11-unix -v "$HOME/.Xauthority:/root/.Xauthority:rw" \
-  -v "$HOME/Android/Sdk:/root/Android/Sdk" -v "$HOME/.AndroidStudio3.5:/root/.AndroidStudio3.5" \
+  -v "$HOME/Android/Sdk:/root/Android/Sdk" -v "$HOME/.AndroidStudio3.4:/root/.AndroidStudio3.4" \
   -v "$HOME/workspace/src:/root/src" \
-   purik/android-studio:latest /opt/android-studio/bin/studio.sh
+   purik/android-studio:latest \
+   /opt/android-studio/bin/studio.sh
