2c8d0f0ea8be2d372204ede1690ff1c553bc7e4d
Pavel Minenkov <minikspb@gmail.com>
2020-05-09 01:07:41 +0300

    Avoid commiting stopped container to dest_image by hands

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-docker run -it --net=host --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" purik/android-studio:latest /android-studio/bin/studio.sh
+
+DST_IMAGE=$1
+
+if [ -z ${DST_IMAGE} ]; then echo "Destination image should be specified. Example: $0 myrepo/android-studio:x" && exit; else echo "Destination image: $DST_IMAGE"; fi
+
+# clean old containers
+docker rm android_studio_container > /dev/null || echo ''
+
+# Run Android-Studio with empty configurations
+docker run -it --name=android_studio --net=host --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" purik/android-studio:latest /android-studio/bin/studio.sh ; \
+  docker commit android_studio_container $DST_IMAGE
