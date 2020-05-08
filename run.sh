012cf97c6f2500634be1418f647ff253d67838b7
Pavel Minenkov <minikspb@gmail.com>
2020-05-09 02:10:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ if [ -z ${DST_IMAGE} ]; then echo "Destination image should be specified. Exampl
 docker rm android_studio_container > /dev/null || echo ''
 
 # Run Android-Studio with empty configurations
-docker run -it --name=android_studio --net=host --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" purik/android-studio:latest /android-studio/bin/studio.sh ; \
+docker run -it --name=android_studio_container --net=host --env="DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix -v="$HOME/.Xauthority:/root/.Xauthority:rw" purik/android-studio:latest /android-studio/bin/studio.sh ; \
   docker commit android_studio_container $DST_IMAGE
